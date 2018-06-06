1d71bf2e2456c9f83f44ee2c5fc1e7a7969f62d7
sgibson47 <sgibson47@gmail.com>
2018-06-06 18:45:24 -0500

    deleted hard coded showings

--- lib/upcoming_movies_in_the_park/showing.rb
+++ lib/upcoming_movies_in_the_park/showing.rb
@@ -47,47 +47,5 @@ class UpcomingMoviesInThePark::Showing
     end
     @@all<<thing
   end 
-
-  show_1 = UpcomingMoviesInThePark::Showing.new
-  show_1.name = "Ferris Bueller's Day Off"
-  show_1.date = ("\n              Date & Time:\n              Mon, Jun 11, 2018 from 8:30 PM - 10:22 PM\n            ")
-  show_1.park = ("Lincoln Park")
-  show_1.url = "url"
-  @@all << show_1
-
-
-  show_2 = UpcomingMoviesInThePark::Showing.new
-  show_2.name = "The Birdcage"
-  show_2.date =("\n              Date & Time:\n              Mon, Jun 12, 2018 from 7:30 PM - 9:22 PM\n            ")
-  show_2.park = ("Berger Park")
-  show_2.url = "url"
-  @@all << show_2
-
-  show_3 = UpcomingMoviesInThePark::Showing.new
-  show_3.name = "The Princess Bride"
-  show_3.date = ("\n              Date & Time:\n              Mon, Jun 13, 2018 from 7:00 PM - 9:00 PM\n            ")
-  show_3.park = ("Wicker Park")
-  show_3.url = "url"
-  @@all << show_3
-
-  show_4 = UpcomingMoviesInThePark::Showing.new
-  show_4.name = "Rear Window"
-  show_4.date = ("\n              Date & Time:\n              Mon, Jun 12, 2018 from 8:00 PM - 10:00 PM\n            ")
-  show_4.park = ("Lincoln Park")
-  show_4.url = "url"
-  @@all << show_4
-
-  show_5 = UpcomingMoviesInThePark::Showing.new
-  show_5.name = "Finding Nemo"
-  show_5.date = ("\n              Date & Time:\n              Mon, Jun 11, 2018 from 8:15 PM - 10:45 PM\n            ")
-  show_5.park = ("Wicker Park")
-  show_5.url = "url"
-  @@all << show_5
-
-  show_6 = UpcomingMoviesInThePark::Showing.new
-  show_6.name = "Hidden Figures"
-  show_6.date = ("\n              Date & Time:\n              Mon, Jun 13, 2018 from 6:30 PM - 8:22 PM\n            ")
-  show_6.park = ("Berger Park")
-  show_6.url = "url"
-  @@all << show_6
+  
 end
