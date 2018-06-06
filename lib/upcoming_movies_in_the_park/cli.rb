fa9acd7de4fb7648e21ea9c5c301cc9e842f80b4
sgibson47 <sgibson47@gmail.com>
2018-06-06 11:54:25 -0500

    built out #call to take user's input and return list of requested data

--- lib/upcoming_movies_in_the_park/cli.rb
+++ lib/upcoming_movies_in_the_park/cli.rb
@@ -1,7 +1,34 @@
 class UpcomingMoviesInThePark::CLI
 
   def call
-    puts "a list of upcoming movies"
+    puts "\n"
+    puts "Upcoming Movies in Chicago Parks"
+    puts "\n"
+    puts "How would you like to explore movies in the park?"
+    puts "Enter -by name- to see a list of upcoming movies by their names."
+    puts "Enter -by date- to see a list of upcoming dates on which movies are playing in a park."
+    puts "Enter -by park- to see a list of parks in which upcoming movies are playing."
+    puts "Or enter -exit- to quit."
+
+    input = gets.strip
+
+    case input
+    when "by name"
+      puts "1. Rear Window"
+      puts "2. The Princess Bride"
+      puts "3. Ferris Bueller's Day Off"
+    when "by date"
+      puts "1. Jun 6, 2018"
+      puts "2. Jun 7, 2018"
+      puts "3. Jun 8, 2018"
+    when "by park"
+      puts "1. Lincoln Park"
+      puts "2. Wicker Park"
+      puts "3. Millenium Park"
+    end
+
   end
+
+
   
 end
