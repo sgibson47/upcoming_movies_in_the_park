b2e35ab5c8d38c4d617b5d5763b30a4b80981e08
sgibson47 <sgibson47@gmail.com>
2018-06-06 15:11:09 -0500

    add empty array set to @@all, add #save to add instance to @@all, and add #self.all to expose @@all's contents

--- lib/upcoming_movies_in_the_park/gate.rb
+++ lib/upcoming_movies_in_the_park/gate.rb
@@ -1,3 +1,16 @@
 class UpcomingMoviesInThePark::Showing
+  attr_accessor :name, :day, :month, :year, :showings
+
+  @@all = []
+  # date info pulled from web, this'll be the name, then parse it to get desired info
+  #"\n              Date & Time:\n              Mon, Jun 11, 2018 from 8:30 PM - 10:22 PM\n            "
+
+  def save
+    @@all << self
+  end 
+
+  def self.all
+    @@all
+  end 
   
 end
