c044ccb41d5af654f4d00b7e88997f3708ba6644
sgibson47 <sgibson47@gmail.com>
2018-06-06 12:17:33 -0500

    added prompts to by date and by park paths

--- lib/upcoming_movies_in_the_park/cli.rb
+++ lib/upcoming_movies_in_the_park/cli.rb
@@ -14,9 +14,11 @@ class UpcomingMoviesInThePark::CLI
     while input != "exit"
       input = gets.strip
       if input == "by name"
+        puts "\n"
         puts "1. Rear Window"
         puts "2. The Princess Bride"
         puts "3. Ferris Bueller's Day Off"
+        puts "\n"
         puts "Enter a number to get the showing details for that movie"
         puts "or enter back to go back to the main menu."
         
@@ -53,10 +55,15 @@ class UpcomingMoviesInThePark::CLI
         puts "1. Jun 6, 2018"
         puts "2. Jun 7, 2018"
         puts "3. Jun 8, 2018"
+        puts "\n"
+        puts "Enter a number to get a list of movies playing on that date"
+        puts "or enter back to go back to the main menu."
       elsif input == "by park"
         puts "1. Lincoln Park"
         puts "2. Wicker Park"
         puts "3. Millenium Park"
+        puts "Enter a number to get a list of movies playing in that park"
+        puts "or enter back to go back to the main menu."
       elsif input === "exit"
         break
       else
