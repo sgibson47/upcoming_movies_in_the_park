07d721561b95b97edc9e429c74a7d8a0552c378d
sgibson47 <sgibson47@gmail.com>
2018-06-06 12:26:27 -0500

    added a layer to the by park path, now user can retrieve a list of movies playing in a given park

--- lib/upcoming_movies_in_the_park/cli.rb
+++ lib/upcoming_movies_in_the_park/cli.rb
@@ -98,6 +98,40 @@ class UpcomingMoviesInThePark::CLI
         puts "3. Millenium Park"
         puts "Enter a number to get a list of movies playing in that park"
         puts "or enter back to go back to the main menu."
+        input = nil
+        while input != "back"
+          input = gets.strip
+          if input == "1"
+            puts "1. Rear Window"
+            puts "2. The Princess Bride"
+            puts "3. Ferris Bueller's Day Off"
+            puts "\n"
+            puts "Enter another number to get the showing details for that movie"
+            puts "or enter back to go back to the main menu."
+          elsif input == "2"
+            puts "1. The Birdcage"
+            puts "2. Finding Nemo"
+            puts "3. The Goonies"
+            puts "\n"
+            puts "Enter another number to get the showing details for that movie"
+            puts "or enter back to go back to the main menu."
+          elsif input == "3"
+            puts "1. Moana"
+            puts "2. North by Northwest"
+            puts "3. Hidden Figures"
+            puts "\n"
+            puts "Enter another number to get the showing details for that movie"
+            puts "or enter back to go back to the main menu."
+          elsif input == "back"
+            puts "\n"
+            puts "Welcome back to the main menu."
+            puts "Enter -by name- to see a list of upcoming movies by their names."
+            puts "Enter -by date- to see a list of upcoming dates on which movies are playing in a park."
+            puts "Enter -by park- to see a list of parks in which upcoming movies are playing."
+            puts "Or enter -exit- to quit."
+            break
+          end 
+        end
       elsif input === "exit"
         break
       else
