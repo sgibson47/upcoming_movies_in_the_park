d7ef7cd2257a782fa69677543a9ca927c23accae
sgibson47 <sgibson47@gmail.com>
2018-06-06 15:58:09 -0500

    add #time to get time info from date name

--- lib/upcoming_movies_in_the_park/showing.rb
+++ lib/upcoming_movies_in_the_park/showing.rb
@@ -1,7 +1,7 @@
 require_relative './park'
 require_relative './gate'
 class UpcomingMoviesInThePark::Showing
-  attr_accessor :name, :date, :time, :park, :url
+  attr_accessor :name, :date, :park, :url
 
   @@all =[]
 
@@ -15,7 +15,7 @@ class UpcomingMoviesInThePark::Showing
   end
 
   def display_details
-    puts "#{@name} is playing at #{@time}"
+    puts "#{@name} is playing from #{self.time}"
     puts "on #{@date.month} #{@date.day}, #{@date.year} in #{@park.name}."
   end
 
@@ -23,10 +23,14 @@ class UpcomingMoviesInThePark::Showing
     @@all << self
   end
 
+  def time
+    data = self.date.name.split(/, | |\n/)
+    time = "#{data[37]} #{data[38]} to #{data[40]} #{data[41]}"
+  end
+
   show_1 = UpcomingMoviesInThePark::Showing.new
   show_1.name = "Ferris Bueller's Day Off"
   show_1.date = UpcomingMoviesInThePark::Gate.new("\n              Date & Time:\n              Mon, Jun 11, 2018 from 8:30 PM - 10:22 PM\n            ")
-  show_1.time = "8:30 PM - 10:15 PM"
   show_1.park = UpcomingMoviesInThePark::Park.new("Lincoln Park")
   show_1.url = "url"
   @@all << show_1
@@ -35,7 +39,6 @@ class UpcomingMoviesInThePark::Showing
   show_2 = UpcomingMoviesInThePark::Showing.new
   show_2.name = "The Birdcage"
   show_2.date = UpcomingMoviesInThePark::Gate.new("\n              Date & Time:\n              Mon, Jun 11, 2018 from 8:30 PM - 10:22 PM\n            ")
-  show_2.time = "6:30 PM - 8:30 PM"
   show_2.park = UpcomingMoviesInThePark::Park.new("Berger Park")
   show_2.url = "url"
   @@all << show_2
@@ -43,7 +46,6 @@ class UpcomingMoviesInThePark::Showing
   show_3 = UpcomingMoviesInThePark::Showing.new
   show_3.name = "The Princess Bride"
   show_3.date = UpcomingMoviesInThePark::Gate.new("\n              Date & Time:\n              Mon, Jun 11, 2018 from 8:30 PM - 10:22 PM\n            ")
-  show_3.time = "7:30 PM - 9:15 PM"
   show_3.park = UpcomingMoviesInThePark::Park.new("Wicker Park")
   show_3.url = "url"
   @@all << show_3
