aa2e08578fd01dfd253619c1ad2f0a8fd9d902bc
sgibson47 <sgibson47@gmail.com>
2018-06-06 14:41:23 -0500

    set showings'parks to Park objects

--- lib/upcoming_movies_in_the_park/showing.rb
+++ lib/upcoming_movies_in_the_park/showing.rb
@@ -14,7 +14,7 @@ class UpcomingMoviesInThePark::Showing
 
   def display_details
     puts "#{@name} is playing at #{@time}"
-    puts "on #{@date} in #{@park}."
+    puts "on #{@date} in #{@park.name}."
   end
 
   def save
@@ -25,7 +25,7 @@ class UpcomingMoviesInThePark::Showing
   show_1.name = "Ferris Bueller's Day Off"
   show_1.date = "Fri, Jun 1, 2018"
   show_1.time = "8:30 PM - 10:15 PM"
-  show_1.park = "Lincoln Park"
+  show_1.park = UpcomingMoviesInThePark::Park.new("Lincoln Park")
   show_1.url = "url"
   @@all << show_1
 
@@ -34,7 +34,7 @@ class UpcomingMoviesInThePark::Showing
   show_2.name = "The Birdcage"
   show_2.date = "Sat, Jun 2, 2018"
   show_2.time = "6:30 PM - 8:30 PM"
-  show_2.park = "Berger Park"
+  show_2.park = UpcomingMoviesInThePark::Park.new("Berger Park")
   show_2.url = "url"
   @@all << show_2
 
@@ -42,7 +42,7 @@ class UpcomingMoviesInThePark::Showing
   show_3.name = "The Princess Bride"
   show_3.date = "Sun, Jun 3, 2018"
   show_3.time = "7:30 PM - 9:15 PM"
-  show_3.park = "Wicker Park"
+  show_3.park = UpcomingMoviesInThePark::Park.new("Wicker Park")
   show_3.url = "url"
   @@all << show_3
 end
