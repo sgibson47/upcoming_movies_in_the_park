9a5dc8f7dffa62d86871d53cfa7752d05d753db8
sgibson47 <sgibson47@gmail.com>
2018-06-06 18:17:18 -0500

    refactor #by_park_path to utilize #park.list_showings and #showing.display_details

--- lib/upcoming_movies_in_the_park/cli.rb
+++ lib/upcoming_movies_in_the_park/cli.rb
@@ -73,6 +73,7 @@ class UpcomingMoviesInThePark::CLI
       input = gets.strip
       if input.to_i > 0 && input.to_i <= UpcomingMoviesInThePark::Gate.all.length
         date = UpcomingMoviesInThePark::Gate.all[input.to_i - 1]
+        puts "\n"
         date.list_showings
         puts "\n"
         puts "Enter a number to get the showing details for that movie"
@@ -124,10 +125,10 @@ class UpcomingMoviesInThePark::CLI
     input = nil
     while input != "back"
       input = gets.strip
-      if input == "1"
-        puts "1. Rear Window"
-        puts "2. The Princess Bride"
-        puts "3. Ferris Bueller's Day Off"
+      if input.to_i > 0 && input.to_i <= UpcomingMoviesInThePark::Park.all.length
+        park = UpcomingMoviesInThePark::Park.all[input.to_i - 1]
+        puts "\n"
+        park.list_showings
         puts "\n"
         puts "Enter another number to get the showing details for that movie"
         puts "or enter return to return to the list of parks"
@@ -135,114 +136,17 @@ class UpcomingMoviesInThePark::CLI
         input = nil
         while input != "return"
           input = gets.strip
-          if input == "1"
-            puts "details on showing of Rear Window"
+          if input.to_i > 0 && input.to_i <= park.showings.length
+            showing = park.showings[input.to_i - 1]
             puts "\n"
-            puts "Enter another number to get the showing details for that movie"
-            puts "or enter return to return to the list of parks"
-            puts "in which upcoming movies are playing."
-          elsif input == "2"
-            puts "details on showing of The Princess Bride"
-            puts "\n"
-            puts "Enter another number to get the showing details for that movie"
-            puts "or enter return to return to the list of parks"
-            puts "in which upcoming movies are playing."
-          elsif input == "3"
-            puts "details on showing of Ferris Bueller's Day Off"
+            showing.display_details
             puts "\n"
             puts "Enter another number to get the showing details for that movie"
             puts "or enter return to return to the list of parks"
             puts "in which upcoming movies are playing."
           elsif input == "return"
-            puts "1. Lincoln Park"
-            puts "2. Wicker Park"
-            puts "3. Millenium Park"
             puts "\n"
-            puts "Enter a number to get a list of movies playing in that park"
-            puts "or enter back to go back to the main menu."
-            break
-          else 
-            puts "Enter a number to get the showing details for that movie"
-            puts "or enter return to return to the list of parks"
-            puts "in which upcoming movies are playing."
-          end 
-        end
-      elsif input == "2"
-        puts "1. The Birdcage"
-        puts "2. Finding Nemo"
-        puts "3. The Goonies"
-        puts "\n"
-        puts "Enter another number to get the showing details for that movie"
-        puts "or enter return to return to the list of parks"
-        puts "in which upcoming movies are playing."
-        input = nil
-        while input != "return"
-          input = gets.strip
-          if input == "1"
-            puts "details on showing of The Birdcage"
-            puts "\n"
-            puts "Enter another number to get the showing details for that movie"
-            puts "or enter return to return to the list of parks"
-            puts "in which upcoming movies are playing."
-          elsif input == "2"
-            puts "details on showing of Finding Nemo"
-            puts "\n"
-            puts "Enter another number to get the showing details for that movie"
-            puts "or enter return to return to the list of parks"
-            puts "in which upcoming movies are playing."
-          elsif input == "3"
-            puts "details on showing of The Goonies"
-            puts "\n"
-            puts "Enter another number to get the showing details for that movie"
-            puts "or enter return to return to the list of parks"
-            puts "in which upcoming movies are playing."
-          elsif input == "return"
-            puts "1. Lincoln Park"
-            puts "2. Wicker Park"
-            puts "3. Millenium Park"
-            puts "\n"
-            puts "Enter a number to get a list of movies playing in that park"
-            puts "or enter back to go back to the main menu."
-            break
-          else 
-            puts "Enter a number to get the showing details for that movie"
-            puts "or enter return to return to the list of parks"
-            puts "in which upcoming movies are playing."
-          end 
-        end
-      elsif input == "3"
-        puts "1. Moana"
-        puts "2. North by Northwest"
-        puts "3. Hidden Figures"
-        puts "\n"
-        puts "Enter a number to get the showing details for that movie"
-        puts "or enter return to return to the list of parks"
-        puts "in which upcoming movies are playing."
-        input = nil
-        while input != "return"
-          input = gets.strip
-          if input == "1"
-            puts "details on showing of Moana"
-            puts "\n"
-            puts "Enter another number to get the showing details for that movie"
-            puts "or enter return to return to the list of parks"
-            puts "in which upcoming movies are playing."
-          elsif input == "2"
-            puts "details on showing of North by Northwest"
-            puts "\n"
-            puts "Enter another number to get the showing details for that movie"
-            puts "or enter return to return to the list of parks"
-            puts "in which upcoming movies are playing."
-          elsif input == "3"
-            puts "details on showing of Hidden Figures"
-            puts "\n"
-            puts "Enter another number to get the showing details for that movie"
-            puts "or enter return to return to the list of parks"
-            puts "in which upcoming movies are playing."
-          elsif input == "return"
-            puts "1. Lincoln Park"
-            puts "2. Wicker Park"
-            puts "3. Millenium Park"
+            park.list_showings
             puts "\n"
             puts "Enter a number to get a list of movies playing in that park"
             puts "or enter back to go back to the main menu."
