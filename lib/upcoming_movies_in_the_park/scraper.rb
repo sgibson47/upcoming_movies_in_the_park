c27484c4e76c698f3c43c76170d6bde221818f03
sgibson47 <sgibson47@gmail.com>
2018-06-06 18:26:42 -0500

    add #get_showing_urls

--- lib/upcoming_movies_in_the_park/scraper.rb
+++ lib/upcoming_movies_in_the_park/scraper.rb
@@ -1,3 +1,12 @@
 class UpcomingMoviesInThePark::Scraper
-  
+
+  def get_showing_urls
+    day = Date.today
+    doc = Nokogiri::HTML(open("https://www.chicagoparkdistrict.com/events/night-out-in-the-parks?field_event_category=404&field_event_start_date=#{day.year}-#{day.month}-#{day.day}&field_event_end_date=&field_event_movie_title=&field_event_movie_rating=All"))
+
+    doc2 = doc.css("div.view-content")
+
+    arr = doc2.children.css("div.views-field h2 a").map{ |x| x.attribute('href').value}
+  end
+
 end
