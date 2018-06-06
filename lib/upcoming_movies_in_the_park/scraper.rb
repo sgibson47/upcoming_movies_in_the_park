88c785ed78d928be77cc458129ae6de232280ba4
sgibson47 <sgibson47@gmail.com>
2018-06-06 18:41:08 -0500

    add #get_page and refactor #get_showing_urls and #get_showing_details to use same

--- lib/upcoming_movies_in_the_park/scraper.rb
+++ lib/upcoming_movies_in_the_park/scraper.rb
@@ -5,9 +5,13 @@ class UpcomingMoviesInThePark::Scraper
     @showings =[]
   end
 
+  def get_page(url)
+    Nokogiri::HTML(open(url))
+  end
+
   def get_showing_urls
     day = Date.today
-    doc = Nokogiri::HTML(open("https://www.chicagoparkdistrict.com/events/night-out-in-the-parks?field_event_category=404&field_event_start_date=#{day.year}-#{day.month}-#{day.day}&field_event_end_date=&field_event_movie_title=&field_event_movie_rating=All"))
+    doc = get_page("https://www.chicagoparkdistrict.com/events/night-out-in-the-parks?field_event_category=404&field_event_start_date=#{day.year}-#{day.month}-#{day.day}&field_event_end_date=&field_event_movie_title=&field_event_movie_rating=All")
 
     doc2 = doc.css("div.view-content")
 
@@ -16,7 +20,7 @@ class UpcomingMoviesInThePark::Scraper
 
   def get_showing_details
     self.urls.each do |url|
-      doc = Nokogiri::HTML(open("https://www.chicagoparkdistrict.com/#{url}")
+      doc = get_page("https://www.chicagoparkdistrict.com/#{url}")
       showing_hash ={}
       showing_hash[:name] = doc.css("").text
       showing_hash[:date] = doc.css("").text
