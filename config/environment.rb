a0f79c94c278974e5c9fcc5a1807eec7ce5e2c88
sgibson47 <sgibson47@gmail.com>
2018-06-07 10:50:57 -0500

    required colorize

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,7 @@
 require 'nokogiri'
 require 'open-uri'
 require 'pry'
+require 'colorize'
 
 require_relative '../lib/upcoming_movies_in_the_park/version'
 require_relative '../lib/upcoming_movies_in_the_park/cli'
