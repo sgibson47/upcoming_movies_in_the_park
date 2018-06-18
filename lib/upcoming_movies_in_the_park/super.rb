2a023063f8ef97a6c46d50685ed25383ca068c67
sgibson47 <sgibson47@gmail.com>
2018-06-18 12:07:17 -0500

    move #list_showings into diff module b/c Showing class doesn't useit

--- lib/upcoming_movies_in_the_park/super.rb
+++ lib/upcoming_movies_in_the_park/super.rb
@@ -6,17 +6,17 @@ module UpcomingMoviesInThePark::Super
       self.class.all << self
     end 
 
-    def list_showings
-    self.showings.each_with_index {|showing, i| 
-      puts "#{i +1}. #{showing.name}".colorize(:cyan)}
-    end
-
   end
 
   module GateParkInstanceMethods
     
     def add_showing(showing)
       self.showings << showing
+    end
+
+    def list_showings
+      self.showings.each_with_index {|showing, i| 
+        puts "#{i +1}. #{showing.name}".colorize(:cyan)}
     end
   
   end
